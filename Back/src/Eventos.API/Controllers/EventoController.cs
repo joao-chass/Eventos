018150b4ae66a240478fffd33bade110dc35d74b
joao-chass <joaoborges07@hotmail.com>
2021-03-17 23:26:13 -0300

    Migration e add sqlite

--- Back/src/Eventos.API/Controllers/EventoController.cs
+++ Back/src/Eventos.API/Controllers/EventoController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Eventos.API.Data;
 using Eventos.API.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
@@ -12,44 +13,25 @@ namespace Eventos.API.Controllers
     [Route("api/[controller]")]
     public class EventoController : ControllerBase
     {
-        public IEnumerable<Evento> _evento = new Evento[]
-            {
-               new Evento()
+        private readonly DataContext _context;
+
+        public EventoController(DataContext context)
         {
-            EventoId = 1,
-                Tema = "Angular 11 e .net 5",
-                Local = "S",
-                Lote = "1º lote",
-                QtdPessoas = 250,
-                DataEnvento = DateTime.Now.AddDays(2).ToString("dd/MM/yyyy"),
-                ImagemURL = "fots"
-               },
-                new Evento()
-        {
-            EventoId = 2,
-                Tema = "Angular 11 e .net 5",
-                Local = "S",
-                Lote = "1º lote",
-                QtdPessoas = 250,
-                DataEnvento = DateTime.Now.AddDays(2).ToString(),
-                ImagemURL = "fots"
-               }
-    };
-    public EventoController()
-        {
-           
+            _context = context;
         }
 
         [HttpGet]
         public IEnumerable<Evento> Get()
         {
-            return _evento;
+            return _context.Eventos;
         }
 
         [HttpGet("{id}")]
-        public IEnumerable<Evento> GetById(int id)
+        public Evento GetById(int id)
         {
-            return _evento.Where(evento => evento.EventoId == id);
+            return _context.Eventos.FirstOrDefault(
+                evento => evento.EventoId == id
+                );
         }
 
         [HttpPost]
---
+++ Back/src/Eventos.API/Data/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using Eventos.API.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Eventos.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+        public DbSet<Evento> Eventos { get; set; }
+    }
+}
